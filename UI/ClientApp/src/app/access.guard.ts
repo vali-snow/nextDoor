2c07662e382e60c78bce9adf0a757541b4fa2ae7
vali-snow <valentin.sarghi@gmail.com>
2019-10-12 22:30:01 +0300

    playing around with routes and a separate login component placeholder

---
+++ UI/ClientApp/src/app/access.guard.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, CanActivateChild, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccessGuard implements CanActivate, CanActivateChild {
+  constructor (private router: Router) {
+    
+  }
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const requiresLogin = next.data.requiresLogin || false;
+    if (requiresLogin) {
+      this.router.navigate(['login']);
+    }
+    return true;
+  }
+  canActivateChild(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    const requiresLogin = next.data.requiresLogin || false;
+    if (requiresLogin) {
+      this.router.navigate(['login']);
+    }
+    return true;
+  }
+  
+}
--- UI/ClientApp/src/app/main/app.module.ts
+++ UI/ClientApp/src/app/main/app.module.ts
@@ -2,31 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { AccessGuard } from '../access.guard';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full', data: { requiresLogin: true }, canActivate: [ AccessGuard ] },
+  { path: '1', component: HomeComponent, canActivate: [ AccessGuard ] },
+  { path: 'login', loadChildren: () => import('../login/login.module').then(m => m.LoginModule) }
+  //{ path: '**', component: NotFoundComponent }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-    ])
+    RouterModule.forRoot(
+      routes, {enableTracing: true}
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
