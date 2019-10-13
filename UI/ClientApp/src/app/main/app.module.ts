7067cf016996d269a86c38d96b42a037ccea75a1
vali-snow <valentin.sarghi@gmail.com>
2019-10-13 12:32:21 +0300

    100% height on the placeholder

--- UI/ClientApp/src/app/main/app.module.ts
+++ UI/ClientApp/src/app/main/app.module.ts
@@ -3,11 +3,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { RouterModule, Routes } from '@angular/router';
+import { AppMaterialModule } from '../app-material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { AccessGuard } from '../access.guard';
+
+
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full', data: { requiresLogin: true }, canActivate: [ AccessGuard ] },
@@ -24,11 +29,14 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    AppMaterialModule,
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot(
       routes, {enableTracing: true}
-    )
+    ),
+    BrowserAnimationsModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
